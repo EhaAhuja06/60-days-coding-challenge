b779a90688fcdc6bfe084f2195b1152a5635281f
EhaAhuja06 <ehaahuja1406@gmail.com>
2025-09-22 02:26:02 +0530

    Create Day 7.py
    today I started with some basic yet powerful topics on python

---
+++ Day 7.py
@@ -0,0 +1,21 @@
+#Hackerrank problem on topic loops
+if _name_ == '_main_':
+    n = int(input())
+    for i in range(0,n):
+        sq=i*i;
+        print(sq)
+
+#Hackerrank problem topic arithmetic operators
+if _name_ == '_main_':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+#Hackerrank problem on topic division
+if _name_ == '_main_':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
