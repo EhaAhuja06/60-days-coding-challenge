500121b81051795a6aab52360a4f10c9a2eb082f
EhaAhuja06 <ehaahuja1406@gmail.com>
2025-09-24 01:04:59 +0530

    Create Day 9.py
    Sort 0's, 1's and 2's problem solution

---
+++ Day 9.py
@@ -0,0 +1,35 @@
+#gfg problem sort 0's, 1's and 2's
+void sort012(int arr[], int n) 
+{
+    int c0=0, c1=0, c2=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            c0+=1;
+        }
+        else if(arr[i]==1)
+        {
+            c1+=1;
+        }
+        else
+        {
+            c2+=1;
+        }
+    }
+    int idx=0;
+    for(int i=0;i<c0;i++)
+    {
+        arr[idx++]=0;
+    }
+    for(int i=0;i<c1;i++)
+    {
+        arr[idx++]=1;
+    }
+    for(int i=0;i<c2;i++)
+    {
+        arr[idx++]=2;
+    }
+    return 0;
+}
+    
