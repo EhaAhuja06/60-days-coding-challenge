1bc81e68eccb62cb4e8bff15316337825777f4ed
EhaAhuja06 <ehaahuja1406@gmail.com>
2025-10-02 02:42:49 +0530

    Create Day 17.cpp

---
+++ Day 17.cpp
@@ -0,0 +1,20 @@
+//leetcode problem 58 length of last word
+class Solution {
+public:
+    int lengthOfLastWord(string s) 
+    {
+        int count = 0; 
+        int i = s.length() - 1;
+        while (i >= 0 && s[i] == ' ') 
+        {
+            i--;
+        }
+        while (i >= 0 && s[i] != ' ') 
+        {
+            count++;
+            i--;
+        }
+
+        return count;
+    }
+};
