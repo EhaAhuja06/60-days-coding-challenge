316b62dc8195ca1db894ee1b9dd5c248c3220bc7
EhaAhuja06 <ehaahuja1406@gmail.com>
2025-10-21 03:51:09 +0530

    Create Day 34.cpp
    solved leetcode problem 292

---
+++ Day 34.cpp
@@ -0,0 +1,11 @@
+//Leetcode problem 292 Nim game
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n%4==0)
+        {
+            return false;
+        }
+        return true;
+    }
+};
