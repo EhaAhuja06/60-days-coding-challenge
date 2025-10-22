a8e70d2995dbba9559fac74ff57a233de23f67e6
EhaAhuja06 <ehaahuja1406@gmail.com>
2025-10-23 02:03:10 +0530

    Create Day 36.cpp
    leetcode problem 108

---
+++ Day 36.cpp
@@ -0,0 +1,20 @@
+//Leetcode problem 108 Convert Sorted array to bst
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+    
+private:
+    TreeNode* helper(vector<int>& nums, int low, int high) {
+        if (low > high) return nullptr;
+        
+        int mid = low + (high - low) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        
+        root->left = helper(nums, low, mid - 1);
+        root->right = helper(nums, mid + 1, high);
+        
+        return root;
+    }
+};
